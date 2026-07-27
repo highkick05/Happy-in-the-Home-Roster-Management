99a68d0ad9dd686e849e4d7af84b98e3a972ff04
Matt Willis <mattwillis02@gmail.com>
2026-07-27 02:01:22 +0000

    chore: remove test scripts

--- check_files_db2.cjs
+++
@@ -1,4 +0,0 @@
-const Database = require('better-sqlite3');
-const db = new Database('data/dev-database.sqlite');
-const info = db.prepare("SELECT * FROM files ORDER BY id DESC LIMIT 5").all();
-console.log(JSON.stringify(info, null, 2));
