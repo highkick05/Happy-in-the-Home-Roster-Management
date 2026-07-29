c17644ddae94acdfa91952562e3ee772389c6cd7
Matt Willis <mattwillis02@gmail.com>
2026-07-29 12:03:35 +0000

    chore: update generated service worker

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-8d0d8005'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.vtfl4t8eq8g"
+    "revision": "0.5oa2919toto"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
