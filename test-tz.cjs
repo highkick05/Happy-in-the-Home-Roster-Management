7044abc0cf6a345016d4fb6dc772d7b919bdea5d
Matt Willis <mattwillis02@gmail.com>
2026-08-07 13:55:08 +0000

    Remove temporary testing scripts

--- test-tz.cjs
+++
@@ -1,3 +0,0 @@
-const { fromZonedTime } = require('date-fns-tz');
-const d = fromZonedTime('2026-01-29T09:00:00', 'Australia/Perth');
-console.log(d.toISOString());
--- test-tz.js
+++
@@ -1,3 +0,0 @@
-import { fromZonedTime } from 'date-fns-tz';
-const d = fromZonedTime('2026-08-07T09:00:00', 'Australia/Perth');
-console.log(d.toISOString());
