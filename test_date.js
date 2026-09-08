d6727610a752b6b5e453cdbb4a4d3d69f2ecf4ee
mattwillis02 <mattwillis02@gmail.com>
2026-09-08 02:25:32 +0000

    clean: remove patch scripts

--- test_date.js
+++
@@ -1,9 +0,0 @@
-let invoiceDate = "2026-06-10";
-let displayDate = invoiceDate;
-if (displayDate && displayDate.includes("-")) {
-    const parts = displayDate.split("-");
-    if (parts.length === 3 && parts[0].length === 4) {
-        displayDate = `${parts[2]}-${parts[1]}-${parts[0]}`;
-    }
-}
-console.log(displayDate);
